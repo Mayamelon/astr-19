31729efe99cccddb16125757c88f0a7821e27966
Cole L <94574122+Mayamelon@users.noreply.github.com>
2023-09-29 16:46:54 -0700

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,15 @@
+#this program will print my name and pronouns and two sentences about me
+
+#this function tells the computer to print my name and pronouns and two sentences about me
+def PrintName():
+	print("My name is Cole Lewis and my pronouns are He/Him!")
+	print("My favorite movie is 2001: a Space Odyssey.")
+	print("My favorite food is chocolate.\n:)")
+
+#this defines the main function for my program
+def main():
+	PrintName()
+
+#This executes first
+if __name__ == "__main__":
+	main()
